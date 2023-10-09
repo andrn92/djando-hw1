2bea05974674c0b3e7c39b29b78d80f072a99b05
andrn92 <georgkartashov88@gmail.com>
2023-10-09 17:27:05 +0300

    add prefetch_related

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,9 +6,9 @@ from .models import Student, Teacher
 
 def students_list(request):
     template = 'school/students_list.html'
-    students = Student.objects.all()
-    context = {'students': students}
+    students = Student.objects.all().prefetch_related('teachers')
 
+    context = {'students': students}
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
